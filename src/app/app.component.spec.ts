b68c4317df74b13b670cd3ed927477bd240953e6
Zsolt Meszarovics <zmeszarovics@lhediscovery.com>
2018-07-26 15:14:45 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,39 @@
 import { TestBed, async } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
+import { UserDetailsComponent } from "./user-details.component";
+import { LoginServiceConfig } from "./social-login/login-service-config";
+import { SocialLoginModule } from "./social-login/social-login.module";
+
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, UserDetailsComponent
       ],
+      imports: [
+        SocialLoginModule
+      ],
+      providers: [{
+        provide: LoginServiceConfig,
+        useValue: {
+          services: {}
+        }
+      }]
     }).compileComponents();
   }));
+
   it("should create the app", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual("Angular-Social-Sign-In");
   }));
+
   it("should render title in a h1 tag", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
